6027dd16b4252b3f9160b5fc251b8402b14b0f87
gayashan4lk <gayashan4lk@gmail.com>
2025-10-19 20:36:31 +0530

    throw exceptions

--- exception-handling-basics/src/main/java/org/example/Main.java
+++ exception-handling-basics/src/main/java/org/example/Main.java
@@ -1,15 +1,36 @@
 package org.example;
 
+import com.sun.tools.jconsole.JConsoleContext;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
         // ArithmeticException
-//        var result = MathOps.divide(10, 0);
-//        System.out.println(result);
+        /*var result = MathOps.divide(10, 0);
+        System.out.println(result);*/
 
-        // arrays
-        ArrayOps.mockArrayOperation();
+        /*try{
+            var result2 = MathOps.divideBetter(10, 0);
+            System.out.println("result2 = " + result2);
+        } catch (Exception e){
+            System.out.println("Catch exception: " + e.getMessage());
+            System.out.println("Exception type: " + e.getClass());
+        }*/
+
+        try{
+            var result3 = MathOps.multiply();
+            System.out.println("Result3 = " + result3);
+        } catch (Exception e){
+            System.out.println("Exception: " + e + "\nMessage: " + e.getMessage() + "\nStackTrace: " + Arrays.toString(e.getStackTrace()));
+        }
+
+
+
+        // Array Exception
+//        ArrayOps.mockArrayOperation();
     }
 }
 
--- exception-handling-basics/src/main/java/org/example/MathOps.java
+++ exception-handling-basics/src/main/java/org/example/MathOps.java
@@ -12,4 +12,21 @@ public class MathOps {
         }
 
     }
+
+    public static int divideBetter(int dividend, int divisor){
+        try{
+            var result = dividend / divisor;
+            System.out.println("inside the function:" + result);
+            return result;
+        } catch (Exception e){
+            System.out.println("Exception occurred: " + e.getMessage());
+            System.out.println("Exception type: " + e.getClass());
+            throw e;
+        }
+    }
+
+    public static int multiply() throws UnsupportedOperationException {
+        System.out.println("inside the function");
+        throw new UnsupportedOperationException("Multiply method is not implemented yet");
+    }
 }
